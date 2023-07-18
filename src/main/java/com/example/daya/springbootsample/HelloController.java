b3726ee431005f78dc8d66975930109233f69a98
dmnglor <35909263+dmnglor@users.noreply.github.com>
2023-07-18 13:21:33 +0530

    Update HelloController.java

--- src/main/java/com/example/daya/springbootsample/HelloController.java
+++ src/main/java/com/example/daya/springbootsample/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 	
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello to docker-jenkins- spring boot integration"; 
+		return "Hello to docker-jenkins integration"; 
 	}
 	
 
